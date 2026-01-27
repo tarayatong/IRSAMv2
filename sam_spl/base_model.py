f457ad09cadb696d8e7bb6f18879cf62add29928
tarayatong <2295122900@qq.com>
2026-01-27 14:33:50 +0800

    s0: embed-wt*pc, s123: embed+wt*(pt-pc)+wci*(pc-pt), cosloss*0.5

--- sam_spl/base_model.py
+++ sam_spl/base_model.py
@@ -160,7 +160,10 @@ class EmbeddingOptimizer(nn.Module):
         clt_proj = (w_c[..., None, None] * embedding).sum(dim=1, keepdim=True)
         target_mask = self.upsample(tgt_proj)
         clutter_mask = self.upsample(clt_proj)
-        corrected_embedding = embedding - w_t[..., None, None] * clt_proj - w_c_ir[..., None, None] * tgt_proj
+        if self.use_proj:
+            corrected_embedding = embedding + w_t[..., None, None] * (tgt_proj - clt_proj) + w_c_ir[..., None, None] * (clt_proj - tgt_proj)
+        else:
+            corrected_embedding = embedding - w_t[..., None, None] * clt_proj
         
         return_dict = {
             "target_mask": target_mask,
